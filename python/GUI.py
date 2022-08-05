d5889b969641d17cbc16d7c69610070bd3327f21
Guillaume Lemonnier <22007629@etu.unicaen.fr>
2022-08-05 15:04:38 +0200

    avancement alpha gui en python

--- python/GUI.py
+++ python/GUI.py
@@ -20,32 +20,68 @@ def initList() -> list:
     print(ingredients)
     return ingredients
 
-def canvaAddIngredient() -> Tk.Canvas:
+def help() -> None:
+    print("Aide")
+
+def ajoutPhoto() -> None:
+    print("Ajout d'une photo")
+
+def menu(root: Tk.Tk) -> None:
+    menu = Tk.Menu(root)
+    root.config(menu=menu)
+    filemenu = Tk.Menu(menu)
+    menu.add_cascade(label="File", menu=filemenu)
+    filemenu.add_command(label="Open Picture", command=lambda: ajoutPhoto())
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command=root.quit)
+    helpmenu = Tk.Menu(menu)
+    menu.add_cascade(label="Help", menu=helpmenu)
+    helpmenu.add_command(label="About...", command=lambda: help())
+    return menu
+
+def addListeIngredient() -> Tk.Listbox:
     ingredients = initList()
-    CANVAS = Tk.Canvas(width=800, height=600, bg='white')
-    CANVAS.create_text(400, 300, text='Hello World!', font=('Arial', 30))
-    # ajouter une liste defilante de mots
     LISTE = Tk.Listbox(width=50, height=10)
     for i in ingredients:
         LISTE.insert(Tk.END, i)
     LISTE.pack()
-    AJOUTER_INGREDIENT_LISTE = Tk.Button(text='Ajouter un ingredient', command=lambda: ajouter_ingredient(LISTE.get(), ingredients))
-    AJOUTER_INGREDIENT_LISTE.pack()
-    # ajouter un champ de texte
-    ECRIRE_INGREDIENT = Tk.Entry(width=50)
-    ECRIRE_INGREDIENT.pack()
-    # ajouter un bouton
-    AJOUTER_INGREDIENT_WRITE = Tk.Button(text='Envoyer Ingredient', command=lambda: ajouter_ingredient(ECRIRE_INGREDIENT.get(), ingredients))
-    AJOUTER_INGREDIENT_WRITE.pack()
+    return LISTE
+
+def addCanvasRecette(filename: str) -> Tk.Canvas:
+    CANVAS = Tk.Canvas(width=500, height=500)
+    file = open(filename)
+    CANVAS.create_text(250, 250, text=file.read())
+    file.close()
+    CANVAS.pack()
     return CANVAS
 
-def main() -> None:
-    # Liste des ingredients présent
+def addButtonAddIngredient(LISTE: Tk.Listbox, ingredients: list) -> Tk.Button:
+    AJOUTER_INGREDIENT_LISTE = Tk.Button(text='Ajouter un ingredient', command=lambda: ajouter_ingredient(LISTE.get(), ingredients))
+    AJOUTER_INGREDIENT_LISTE.pack()
+    return AJOUTER_INGREDIENT_LISTE
 
+def addButtonAddIngredient(ENTRY: Tk.Entry, ingredients: list) -> Tk.Button:
+    AJOUTER_INGREDIENT_LISTE = Tk.Button(text='Ajouter un ingredient', command=lambda: ajouter_ingredient(ENTRY.get(), ingredients))
+    AJOUTER_INGREDIENT_LISTE.pack()
+    return AJOUTER_INGREDIENT_LISTE
+
+def addEntryCase() -> Tk.Entry:
+    ENTRY = Tk.Entry(width=50)
+    ENTRY.pack()
+    return ENTRY
+
+def main() -> None:
     # Fenêtre de l'interface
+    ingredients = []
+
     root = Tk.Tk()
     root.title("Recherche d'ingrédients")
-    canva = canvaAddIngredient().pack()
+    menu(root)
+    INGREDIENT = addListeIngredient()
+    AJOUTER_INGREDIENT_LISTE = addButtonAddIngredient(INGREDIENT, ingredients)
+    ENTRY = addEntryCase()
+    AJOUTER_INGREDIENT_ENTRY = addButtonAddIngredient(ENTRY, ingredients)
+    RECETTE = addCanvasRecette("recettes/hamburger.csv")
     root.mainloop()
     
 
